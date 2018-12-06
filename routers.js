6e631ae3108d6ed3d2792f86ff983a05ad618328
Matthew Woodroffe <woodroffe.matt@mail.com>
2018-12-06 17:43:46 +0800

    061218 17:45 Authentication JSON implimented

--- routers.js
+++ routers.js
@@ -1,22 +1,36 @@
+const auth = require('./authentication');
+
 module.exports = function(app) {
+    //get request for index page
     app.get("/", (req, res) => {
         res.render('index')
     })
+    //post request for index page login.
     app.post("/", (req, res)=>{
-        console.log(req.body)
+        let authPost = new auth(req, res);
+        if(req.body.hasOwnProperty('loginEmail') === true) {
+            authPost.login();
+        } else if (req.body.hasOwnProperty('email') === true) {
+            authPost.signup();
+        }
     })
+    //get request for profile page
     app.get("/profile_page", (req, res) => {
         res.render('profile_page')
     })
+    //get request for question create page
     app.get("/question_create", (req, res) => {
         res.render('question_create')
     })
+    //post request for question create page
     app.post("/question_create", (req, res)=>{
         console.log(req.body)
     })
-    app.get("/quiz-create", (req, res) => {
+    //get request for quiz create page
+    app.get("/quiz_create", (req, res) => {
         res.render('quiz_create')
     })
+    //get request for results page
     app.get("/results", (req, res) => {
         res.render('results')
     })
